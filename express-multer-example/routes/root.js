7304fe88d35d983e5b79322352445f8ad3913efc
Axel Duch <axlduch@gmail.com>
2015-03-27 16:37:35 +0100

    Working multer upload example

--- express-multer-example/routes/root.js
+++ express-multer-example/routes/root.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var path = require('path');
 var route = express.Router();
-var test = require('./test.js');
 
 route.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-route.use('/test', require('./test.js'));
+route.use('/upload', require('./upload.js'));
 
 module.exports = route;
