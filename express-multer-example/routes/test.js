f233a69a10cc218305974f5e75656e40aa6ee914
Axel Duch <axlduch@gmail.com>
2015-03-27 16:36:39 +0100

    Remove useless test.js from multer example

--- express-multer-example/routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var route = express.Router();
-
-route.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../views/test.html'));
-});
-
-module.exports = route;
