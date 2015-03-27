b2d04c7a3812be31d041f35d812868ec1523079e
Axel Duch <axlduch@gmail.com>
2015-03-27 15:32:13 +0100

    Make a small navigation with routes

--- express-example/routes/test.js
+++ express-example/routes/test.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var path = require('path');
 var route = express.Router();
 
 route.get('/', function (req, res) {
-    res.send('Hi from test');
+    res.sendFile(path.join(__dirname, '../views/test.html'));
 });
 
 module.exports = route;
